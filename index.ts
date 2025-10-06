3f93824af1b598dcd98b275a169b640eb9b6c58b
Austin Merrick <austinmerrick@onsclom.net>
2025-10-06 16:57:17 -0700

    2GB of 10MB chunks w/out archive

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { $ } from "bun";
 
-for (let i = 1; i <= 100; i++) {
-  await $`dd if=/dev/urandom of=bigfile${i}.txt bs=1M count=20`;
+for (let i = 1; i <= 200; i++) {
+  await $`dd if=/dev/urandom of=bigfile${i}.txt bs=1M count=10`;
 }
