97a2f3151559c554e290e9b5d0bed34403b08c8b
Austin Merrick <austinmerrick@onsclom.net>
2025-10-07 10:08:42 -0700

    upload 2000 1MB files

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { $ } from "bun";
 
-for (let i = 1; i <= 200; i++) {
-  await $`dd if=/dev/urandom of=bigfile${i}.txt bs=1M count=10`;
+for (let i = 1; i <= 2000; i++) {
+  await $`dd if=/dev/urandom of=bigfile${i}.txt bs=1M count=1`;
 }
